d4b454e29a49dbc0809a0ccb4656d17f1a36cb8f
Muhammad Hasnain <74878751+hasaninb6340@users.noreply.github.com>
2020-12-13 12:18:30 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 
 
-import uvicorn ##ASGI
+import uvicorn 
 from fastapi import FastAPI
 
-# 2. Create the app object
 app = FastAPI()
 
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
-#uvicorn main:app --reload
+
 
 
 
